03e6b095663562d6edd9efdb88dedad025f36661
andrei gheta <andreigheta77@gmail.com>
2026-03-25 15:06:53 +0200

    Add repositories

---
+++ src/main/java/org/andreigheta/expensetracker/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package org.andreigheta.expensetracker.repository;
+
+import org.andreigheta.expensetracker.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	List<Category> findByUserId(Long userId);
+}
---
+++ src/main/java/org/andreigheta/expensetracker/repository/WalletRepository.java
@@ -0,0 +1,12 @@
+package org.andreigheta.expensetracker.repository;
+
+import org.andreigheta.expensetracker.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+	List<Wallet> findByUserId(Long userId);
+}
